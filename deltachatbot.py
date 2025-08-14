88e68a53bb26d645872f382742df47d1acc6d797
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-14 22:32:31 +0200

    Create deltachatbot.py

---
+++ deltachatbot.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Example echo bot without using hooks
+"""
+
+import logging
+import sys
+
+from deltachat_rpc_client import DeltaChat, EventType, Rpc, SpecialContactId
+
+
+def main():
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    with Rpc() as rpc:
+        deltachat = DeltaChat(rpc)
+        system_info = deltachat.get_system_info()
+        logging.info("Running deltachat core %s", system_info["deltachat_core_version"])
+
+        accounts = deltachat.get_all_accounts()
+        account = accounts[0] if accounts else deltachat.add_account()
+
+        account.set_config("bot", "1")
+        if not account.is_configured():
+            logging.info("Account is not configured, configuring")
+            account.set_config("addr", sys.argv[1])
+            account.set_config("mail_pw", sys.argv[2])
+            account.set_config("mail_server", sys.argv[3])
+            account.set_config("mail_port", sys.argv[4])
+            account.set_config("mail_security", sys.argv[5])
+            account.set_config("send_server", sys.argv[6])
+            account.set_config("send_port", sys.argv[7])
+            account.set_config("send_security", sys.argv[8])
+            account.configure()
+            logging.info("Configured")
+        else:
+            logging.info("Account is already configured")
+            deltachat.start_io()
+
+        def process_messages():
+            for message in account.get_next_messages():
+                snapshot = message.get_snapshot()
+                if snapshot.from_id != SpecialContactId.SELF and not snapshot.is_bot and not snapshot.is_info:
+                    snapshot.chat.send_text(snapshot.text)
+                snapshot.message.mark_seen()
+
+        def echo_qr():
+            qr_code = account.get_qr_code()
+            logging.info("Bot QR Code:\n%s", qr_code)
+
+        echo_qr()
+
+        # Process old messages.
+        process_messages()
+
+        while True:
+            event = account.wait_for_event()
+            if event["kind"] == EventType.INFO:
+                logging.info("%s", event["msg"])
+            elif event["kind"] == EventType.WARNING:
+                logging.warning("%s", event["msg"])
+            elif event["kind"] == EventType.ERROR:
+                logging.error("%s", event["msg"])
+            elif event["kind"] == EventType.INCOMING_MSG:
+                logging.info("Got an incoming message")
+                process_messages()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
