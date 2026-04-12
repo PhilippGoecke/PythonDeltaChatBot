41ecd57dc85e08c4e6361a70285621d3e171dd60
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-12 23:57:17 +0200

    Update podman commands to use new data path

--- podman-run-bot.bash
+++ podman-run-bot.bash
@@ -1,2 +1,2 @@
-podman build --no-cache --rm --volume $(pwd)/data:/home/deltachat/bot -f Containerfile -t deltachatbot:test .
-podman run --interactive --tty --volume $(pwd)/data:/home/deltachat/bot deltachatbot:test
+podman build --no-cache --rm --volume $(pwd)/data:/home/deltachat/data/ -f Containerfile -t deltachatbot:test .
+podman run --interactive --tty --volume $(pwd)/data:/home/deltachat/data/ deltachatbot:test
