5c4469311cbcd0b92e686c2f9f244ef2070305bc
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-13 21:30:07 +0200

    Update podman commands for deltachatbot to use ollama

---
+++ podman-run-bot-ollama.bash
@@ -0,0 +1,2 @@
+podman build --no-cache --rm --volume $(pwd)/data:/home/deltachat/data/ -f Containerfile -t deltachatbot:ollama .
+podman run --interactive --tty --env OLLAMA_HOST=http://host.containers.internal:11434 --env OLLAMA_MODEL=gemma4:31b --volume $(pwd)/data:/home/deltachat/data/ deltachatbot:ollama
